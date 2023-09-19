231cc9bae024653e399bda547c72d08871bfcea7
sariys33 <sariys33@gmail.com>
2023-09-19 17:36:23 +0900

    dv1

--- TWO.java
+++ TWO.java
@@ -5,9 +5,11 @@ public class HelloWorld {
         if(i == 0) {
             System.out.println("Hello World!");
             System.out.println("Hello World!");
+			System.out.println("i == " + i);
         } else {
             System.out.println("else block");
             System.out.println("else block");
+			System.out.println("i == " + i);
         }
     }
 }
