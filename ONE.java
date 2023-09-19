aac8dedc81d2423da06cdceaf87ec055f6693176
sariys33 <sariys33@gmail.com>
2023-09-19 17:46:28 +0900

    one edit

--- ONE.java
+++ ONE.java
@@ -1,6 +1,6 @@
 public class ONE {
     public static void main(String[] args) {
         System.out.println("one");
-        System.out.println("지윤님바보");
+		System.out.println("one edit");
     }
 }
